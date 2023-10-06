d48f6f73942234da9ff00fe55ec975bbf599922e
John Mark Delima <delimajohnmark14@gmail.com>
2023-10-06 14:42:13 +0800

    Add timer funtionality

--- src/script.js
+++ src/script.js
@@ -1,25 +1,29 @@
-import { sherlockQuotes, strangerThingsQuotes, modernFamilyQuotes, gameOfThronesQuotes, moneyHeistQuotes } from "./quotes.js";
+import {
+  sherlockQuotes,
+  strangerThingsQuotes,
+  modernFamilyQuotes,
+  gameOfThronesQuotes,
+  moneyHeistQuotes,
+} from "./quotes.js";
 
-// store the list of words and the index of the word the player is currently typing
+// Constants
+const QUOTE_ELEMENT = document.getElementById("quote");
+const MESSAGE_ELEMENT = document.getElementById("message");
+const TYPED_VALUE_ELEMENT = document.getElementById("typed-value");
+const START_BUTTON = document.getElementById("start");
+const TIME_ALLOTMENT = 20;
+
 let words = [];
 let passedWords = [];
 let wordIndex = 0;
 let wordsTyped = 0;
-
-// the starting time
-let startTime = Date.now();
-
-// page elements
-const quoteElement = document.getElementById('quote');
-const messageElement = document.getElementById('message');
-const typedValueElement = document.getElementById('typed-value');
-const startButton = document.getElementById('start');
 
 let quotes = sherlockQuotes;
 
 const dropdownbutton = document.querySelector('.dropdown label');
 const dropdownItems = document.querySelectorAll('.dropdown-item');
 
+// Event listener for dropdown items
 dropdownItems.forEach(item => {
   item.addEventListener('click', () => {
     // Assign the quotes based on the clicked item's id
@@ -47,95 +51,115 @@ dropdownItems.forEach(item => {
   });
 });
 
-startButton.addEventListener('click', startGame);
-typedValueElement.addEventListener('input', handleInput);
+START_BUTTON.addEventListener('click', startGame);
+TYPED_VALUE_ELEMENT.addEventListener('input', handleInput);
 
 function startGame() {
+
+  TYPED_VALUE_ELEMENT.disabled = false;
+  document.querySelector(".countdown").classList.remove = "hidden";
+  document.getElementById("wpm").innerText = "";
+  timer();
+
   const quoteIndex = Math.floor(Math.random() * quotes.length);
   const quote = quotes[quoteIndex];
 
   // Put the quote into an array of words
   words = quote.split(' ');
 
-  // reset the word index for tracking
+  // Reset word tracking
   wordIndex = 0;
   passedWords = [];
 
   // UI updates
-  startButton.innerText = 'Restart';
-  // Create an array of span elements so we can set a class
+  START_BUTTON.innerText = 'Restart';
   const spanWords = words.map(word => `<span>${word} </span>`);
-  // Convert into a string and set as innerHTML on the quote display
-  quoteElement.innerHTML = spanWords.join('');
-  // Highlight the first word
-  quoteElement.childNodes[0].className = 'highlight';
-  // Clear any prior messages
-  messageElement.innerText = '';
-  messageElement.classList.add('d-none');
+  QUOTE_ELEMENT.innerHTML = spanWords.join('');
+  QUOTE_ELEMENT.childNodes[0].className = 'text-accent-content bg-accent';
 
   // Setup the textbox
-  // Clear the textbox
-  typedValueElement.value = '';
-  // set focus
-  typedValueElement.focus();
-  // set the event handler
-
-  // Start the timer
-  startTime = Date.now();
+  TYPED_VALUE_ELEMENT.value = '';
+  TYPED_VALUE_ELEMENT.focus();
 }
 
 function handleInput() {
-  // Get the current word
   const currentWord = words[wordIndex];
-  // get the current value
-  const typedValue = typedValueElement.value;
+  const typedValue = TYPED_VALUE_ELEMENT.value;
 
   if (typedValue === currentWord && wordIndex === words.length - 1) {
-    // end of sentence
-
-    // Count the last word
+    // End of sentence
+    startGame();
     wordsTyped++;
-
-    // Calculate WPM
-    const elapsedTime = Date.now() - startTime;
-    const minutes = elapsedTime / 1000 / 60; // Convert milliseconds to minutes
-    const wpm = Math.round(wordsTyped / minutes);
-
-    // Display success
-    const message = `CONGRATULATIONS! You finished in ${minutes.toFixed(2)} minutes. Your WPM: ${wpm}`;
-    messageElement.classList.add('d-block');
-    messageElement.classList.remove('d-none');
-    messageElement.innerText = message;
-    quoteElement.childNodes[wordIndex].classList.add = 'text-secondary';
   } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
-    // end of word
+    // End of word
     wordsTyped++;
     passedWords.push(currentWord);
-    // clear the typedValueElement for the new word
-    typedValueElement.value = 'text-neutral-content bg-neutral';
-    // move to the next word
+    TYPED_VALUE_ELEMENT.value = '';
     wordIndex++;
-    // // reset the class name for all elements in the quote
-    // for (const wordElement of quoteElement.childNodes) {
-    //   wordElement.className = '';
-    // }
-    // highlight the new word
-    quoteElement.childNodes[wordIndex].className = 'text-accent-content bg-accent';
+    QUOTE_ELEMENT.childNodes[wordIndex].className = 'text-accent-content bg-accent';
   } else if (currentWord.startsWith(typedValue)) {
-    // currently correct
-    typedValueElement.className = 'input input-accent w-full max-w-xs';
-    startButton.className = 'mt-2 btn-accent px-5 py-2 text-accent-content';
-    quoteElement.childNodes[wordIndex].className = 'text-accent-content bg-accent';
+    // Currently correct
+    TYPED_VALUE_ELEMENT.className = 'input input-accent w-full max-w-xs';
+    START_BUTTON.className = 'btn btn-accent text-accent-content';
+    QUOTE_ELEMENT.childNodes[wordIndex].className = 'text-accent-content bg-accent';
   } else {
-    // error state
-    
-    startButton.className = 'mt-2 btn-primary px-5 py-2 text-primary-content';
-    typedValueElement.className = 'input input-primary w-full max-w-xs';
-    quoteElement.childNodes[wordIndex].className = 'text-primary-content bg-primary';
+    // Error state
+    START_BUTTON.className = 'btn btn-primary text-primary-content';
+    TYPED_VALUE_ELEMENT.className = 'input input-primary w-full max-w-xs';
+    QUOTE_ELEMENT.childNodes[wordIndex].className = 'text-primary-content bg-primary';
   }
 
   for (let i = 0; i < passedWords.length; i++) {
-    // passed word
-    quoteElement.childNodes[i].className = 'text-neutral-content bg-neutral';
+    // Passed word
+    QUOTE_ELEMENT.childNodes[i].className = 'text-neutral-content bg-neutral';
   }
 }
+
+let timerInterval;
+
+function timer() {
+  let remainingTime = TIME_ALLOTMENT;
+
+  const minutesSpan = document.getElementById("minutes");
+  const secondsSpan = document.getElementById("seconds");
+
+  function updateDisplay() {
+    const minutes = Math.floor(remainingTime / 60);
+    const seconds = remainingTime % 60;
+
+    // Update the displayed values using CSS custom properties
+    minutesSpan.style.setProperty("--value", String(minutes).padStart(2, "0"));
+    secondsSpan.style.setProperty("--value", String(seconds).padStart(2, "0"));
+
+    if (remainingTime < 0) {
+      clearInterval(timerInterval);
+      handleGameOver();
+    }
+
+    remainingTime--;
+  }
+
+  // Clear any previous timer intervals
+  clearInterval(timerInterval);
+
+  // Call the updateDisplay function initially
+  updateDisplay();
+
+  // Set an interval to update the display every second
+  timerInterval = setInterval(updateDisplay, 1000);
+}
+
+
+function handleGameOver() {
+  TYPED_VALUE_ELEMENT.disabled = true;
+
+  // Calculate elapsed time in minutes
+  const time = TIME_ALLOTMENT / 60; 
+  const wpm = Math.round(wordsTyped / time);
+ 
+  MESSAGE_ELEMENT.innerText = 'TIMES UP!';
+  document.querySelector(".countdown").classList.add = "hidden";
+  document.getElementById("wpm").innerText = `WPM : ${wpm, time, wordsTyped}`;
+}
+
+
